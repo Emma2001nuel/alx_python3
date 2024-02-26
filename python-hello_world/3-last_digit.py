a1d9abc3f121440f1112e6ad6ff917be2b4b6547
Emmanuel Chalera <emmanuelchalera@gmail.com>
2024-02-26 20:04:34 +0200

    commit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,5 +1,11 @@
 import random
 number = random.randint(-10000, 10000)
 
-print(number)
-print(number)
+if number > 5:
+    print ("the last digit is greater than 5")
+if number == 0:
+    print ("the last digit is 0")
+if number < 6: not 0
+print ("the last digit is less than 6 and not 0") 
+
+
